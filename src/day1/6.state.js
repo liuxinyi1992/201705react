33be9500dd37adc476a70da43e191f3bdb2c3f92
zhufengnodejs <zhufengnodejs@126.com>
2017-08-08 16:37:05 +0800

    属性

--- src/day1/6.state.js
+++ src/day1/6.state.js
@@ -10,16 +10,18 @@ import ReactDOM from 'react-dom';
 class Counter extends React.Component{
   constructor(){
     super();
-    this.state = {number:0};//初始化状态对象
+    this.state = {number:0,name:'zfpx'};//初始化状态对象
   }
   //类上的方法，是所有的实例所公有的 .Counter.prototype.handleClick
   handleClick = ()=>{
      //这样写仅仅是能改变状态
      //this.state.number = this.state.number+1;
     //设置状态对象 1.改变状态 2.重新调用render方法进行渲染
-    this.setState({number:this.state.number+1});
+    //新状态对象会覆盖老状态对象，可不是替换掉,意味着如果新状态对的属性老状态对里没有，会添加此属性，如果有的话会覆盖。如果老的有，新的没有，则保持不变
+    this.setState({number:this.state.number+1,age:9});
   }
   render(){
+    console.log(this.state);
     return (
       <div>
         <p>{this.state.number}</p>
