bb0b5b23210314071f1331bdf83691407d46badc
zhufengnodejs <zhufengnodejs@126.com>
2017-08-08 12:58:01 +0800

    配置.babelrc

--- src/day1/3.component.js
+++ src/day1/3.component.js
@@ -13,9 +13,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 //Hello类继承自React.Component
 /**
- * 1.组件名字必须是以大写字母开头
+ * 1.组件名字必须是以大写字母开头,是为了区分React元素和组件
  * 2.组件内有render方法，返回一个顶级React元素
  * 3.组件的用法和React元素基本一样
+ *
  */
 class Hello extends React.Component{
   //1. 用来指定此组件长什么样子
@@ -24,6 +25,11 @@ class Hello extends React.Component{
     return <div><span>1</span><span>2</span></div>
   }
 }
+/**
+ * 1. 实例化Hello类的实例 let hello = new Hello();
+ * 2. 调用实例上的render方法 .  let ele = hello.render(); ele=div
+ * 3. 把个ele转成真实的DOM元素并且插入到页面中去
+ */
 ReactDOM.render(<Hello/>,document.querySelector('#root'));
 
 
