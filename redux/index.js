9559f125a4e1884c943cdbfc9c08cc14d937c584
zhufengnodejs <zhufengnodejs@126.com>
2017-08-16 16:28:27 +0800

    删除

--- redux/index.js
+++ redux/index.js
@@ -4,3 +4,7 @@
  **/
 import React from 'react';
 import ReactDOM from 'react-dom';
+import store from './store';
+import App from "./components/App";
+
+ReactDOM.render(<App/>,document.querySelector('#root'));
