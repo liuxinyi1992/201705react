5eb33e3d908d3bdacfd666ca0827fa726eefdada
zhufengnodejs <zhufengnodejs@126.com>
2017-08-16 17:03:05 +0800

    删除

--- redux/components/Counter.js
+++ redux/components/Counter.js
@@ -1,10 +1,27 @@
 import React,{Component} from 'react';
-export default class Counter extends Component{
+import {connect} from 'react-redux';
+import * as types from '../store/action-types';
+class Counter extends Component{
   render(){
     return (
       <div>
-        Counter
+        <p>{this.props.number}</p>
+        <button>+</button>
+        <button>-</button>
       </div>
     )
   }
 }
+//把store的状态对象映射当前组件的属性
+let mapStateToProps = (state)=>{
+ return {number:state.counter.number};
+}
+//把dispatch方法映射为属性对象
+let mapDispatchToProps = dispatch=>({
+  increment:()=>dispatch({type:types.INCREMENT})
+})
+//默认导出连接后的组件
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Counter);
