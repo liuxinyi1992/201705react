edfb6c19ad28efd1b347919a284df7ef9e15e783
zhufengnodejs <zhufengnodejs@126.com>
2017-08-16 17:33:38 +0800

    删除

--- redux/components/Todos.js
+++ redux/components/Todos.js
@@ -1,10 +1,22 @@
-import React,{Component} from 'react';
-export default class Todos extends Component{
-  render(){
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+class Todos extends Component {
+  render() {
     return (
       <div>
-        Todos
+        <input type="text"/>
+        <ul>
+
+        </ul>
       </div>
     )
   }
 }
+//把仓库中的状态对象的list映射为当前组件属性对象
+let mapStateToProps = state => ({
+  list:state.todos.list
+})
+export default connect(
+  mapStateToProps,
+  todos
+)(Todos);
