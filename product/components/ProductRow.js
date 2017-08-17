c4ac0de92f18777891310077a81c31dfbb3e602b
zhufengnodejs <zhufengnodejs@126.com>
2017-08-17 11:59:32 +0800

    删除

--- product/components/ProductRow.js
+++ product/components/ProductRow.js
@@ -1,6 +1,7 @@
 import React,{Component} from 'react';
 export default class ProductRow extends React.Component{
   render(){
+    // abcde=>ab<span style={{backgroundColor:'lightgreen'}}>c</span>de
     let name = this.props.product.stocked?this.props.product.name:<span style={{color:'red'}}>{this.props.product.name}</span>;
     return (
      <tr>
